58cf3af08ae5486fab2b5f6c7dd5bd18228dde6d
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-10-29 12:07:37 +0900

    redux-thunk 로 프로미스 다루기

---
+++ src/containers/PostListContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import PostList from "../components/PostList";
+import { getPosts } from "../modules/posts";
+
+function PostListContainer() {
+  const { loading, data, error } = useSelector((state) => state.posts.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error != null) return <div>Error!</div>;
+  if (!data) return null;
+
+  return <PostList posts={data} />;
+}
+
+export default PostListContainer;
---
+++ src/modules/posts.js
@@ -0,0 +1,43 @@
+import * as postsApi from "../api/posts";
+import {
+  createPromiseThunk,
+  handleAsyncActions,
+  reducerUtils,
+} from "../lib/asyncUtils";
+
+/* 액션 타입 */
+
+// 포스트 여러개 조회하기
+const GET_POSTS = "GET_POSTS"; // 요청 시작
+const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS"; // 요청 성공
+const GET_POSTS_ERROR = "GET_POSTS_ERROR"; // 요청 실패
+
+// 포스트 하나 조회하기
+const GET_POST = "GET_POST";
+const GET_POST_SUCCESS = "GET_POST_SUCCESS";
+const GET_POST_ERROR = "GET_POST_ERROR";
+
+export const getPosts = createPromiseThunk(GET_POSTS, postsApi.getPosts);
+export const getPost = createPromiseThunk(GET_POSTS, postsApi.getPostById);
+
+const initialState = {
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial(),
+};
+
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+    case GET_POSTS_SUCCESS:
+    case GET_POSTS_ERROR:
+      const postsReducer = handleAsyncActions(GET_POSTS, "posts");
+      return postsReducer(state, action);
+    case GET_POST:
+    case GET_POST_SUCCESS:
+    case GET_POST_ERROR:
+      const postReducer = handleAsyncActions(GET_POST, "post");
+      return postReducer(state, action);
+    default:
+      return state;
+  }
+}
